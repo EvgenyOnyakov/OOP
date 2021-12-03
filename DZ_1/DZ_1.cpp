f0df27def6b500c150b4cb5a450e42878fd37d88
Evgeny_Onyakov <eonyakov@yandex.ru>
2021-12-03 16:10:48 +0400

    First commit!!!

---
+++ DZ_1/DZ_1.cpp
@@ -0,0 +1,169 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdint>
+using namespace std;
+
+// ================================ ДОМАШНЕЕ ЗАДАНИЕ № 1 ================================
+
+// Создать класс Power, который содержит два вещественных числа.
+// Этот класс должен иметь две переменные - члена для хранения этих вещественных чисел.
+// Еще создать два метода : один с именем set, который позволит присваивать значения переменным, 
+// второй — calculate, который будет выводить результат возведения первого числа в степень второго числа.
+// Задать значения этих двух чисел по умолчанию.
+
+class Power 
+{
+private:
+    double foo;
+    double bar;
+public:
+    Power() : foo(1.1), bar(2.2) {};
+    void setPower(double first, double second)
+    {
+        foo = first;
+        bar = second;
+    }
+    void calculatePower()
+    {            
+        cout << "Pезультат возведения первого числа в степень второго числа: " << pow(foo, bar) << endl;
+    }
+};
+
+// ================================ ДОМАШНЕЕ ЗАДАНИЕ № 2 ================================
+
+// Написать класс с именем RGBA, который содержит 4 переменные - члена типа std::uint8_t: m_red, m_green, m_blue и m_alpha(#include cstdint для доступа к этому типу).
+// Задать 0 в качестве значения по умолчанию для m_red, m_green, m_blue и 255 для m_alpha.
+// Создать конструктор со списком инициализации членов, который позволит пользователю передавать значения для m_red, m_blue, m_green и m_alpha.
+// Написать функцию print(), которая будет выводить значения переменных - членов.
+
+class RGBA
+{
+private:
+    uint8_t m_red, m_green, m_blue, m_alpha;
+public:
+    RGBA() :m_red(0), m_green(0), m_blue(0), m_alpha(0) {};
+    RGBA(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) :m_red(red), m_green(green), m_blue(blue), m_alpha(alpha) {};
+    void print()
+    {
+        cout << "RGBA(" << m_red << "," << m_green << "," << m_blue << "," << m_alpha << ")" << endl;
+    }
+};
+
+// ================================ ДОМАШНЕЕ ЗАДАНИЕ № 3 ================================
+
+// Написать класс, который реализует функциональность стека. Класс Stack должен иметь:
+// private-массив целых чисел длиной 10;
+// private целочисленное значение для отслеживания длины стека;
+// public-метод с именем reset(), который будет сбрасывать длину и все значения элементов на 0;
+// public-метод с именем push(), который будет добавлять значение в стек. push() должен возвращать значение false, если массив уже заполнен, и true в противном случае;
+// public-метод с именем pop() для вытягивания и возврата значения из стека. Если в стеке нет значений, то должно выводиться предупреждение;
+// public-метод с именем print(), который будет выводить все значения стека.
+
+class Stack
+{
+private: 
+    static const int SIZE = 10;
+    int arr[SIZE];    
+    int index;
+public:
+    void reset() 
+    {
+        index = 0;
+        for (size_t i = 0; i < SIZE; i++)
+        {
+            arr[i] = 0;
+        }
+    }
+    bool push(int val)
+    {
+        if (index == 0)
+        {
+            arr[0] = val;
+            index++;
+        }
+        else if (index > 0)
+        {
+            arr[index] = val;
+            index++;
+        }
+        if (index == 10)
+            return false;
+        else
+            return true;
+    }
+    void pop()
+    {
+        if (index == 0)
+            cout << "Ошибка: стек пустой" << endl;
+        else 
+        {
+            arr[index - 1] = 0;
+            index--;
+        }
+    }
+    void print()
+    {
+        cout << "( ";
+        for (size_t i = 0; i < index; i++)
+        {            
+            cout << arr[i] << " ";
+        }
+        cout << ")" << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    // ================================ ДОМАШНЕЕ ЗАДАНИЕ № 1 ================================
+
+    cout << "  ========== Домашнее задание № 1 ========== " << endl << endl;
+    cout << "Введите первое вещественное число: ";
+    double first, second;
+    cin >> first;
+    cout << "Введите второе вещественное число: ";
+    cin >> second;
+    Power FOO;
+    FOO.setPower(first, second);
+    FOO.calculatePower();
+
+    // ================================ ДОМАШНЕЕ ЗАДАНИЕ № 2 ================================
+
+    cout << endl << "  ========== Домашнее задание № 2 ========== " << endl << endl;
+    uint8_t red, green, blue, alpha;
+    cout << "Введите значение переменной red:   ";
+    cin >> red;
+    cout << "Введите значение переменной green: ";
+    cin >> green;
+    cout << "Введите значение переменной blue:  ";
+    cin >> blue;
+    cout << "Введите значение переменной alpha: ";
+    cin >> alpha;
+
+    RGBA mycolor(red, green, blue, alpha);
+    mycolor.print(); 
+
+    // ================================ ДОМАШНЕЕ ЗАДАНИЕ № 3 ================================
+
+    cout << endl << "  ========== Домашнее задание № 3 ========== " << endl << endl;
+    Stack stack;
+    stack.reset();
+    stack.print();
+
+    stack.push(3);
+    stack.push(7);
+    stack.push(5);
+    stack.print();
+
+    stack.pop();
+    stack.print();
+
+    stack.pop();
+    stack.pop();
+    stack.print();
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ DZ_3/DZ_3.cpp
@@ -0,0 +1,344 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <numbers>
+#include <cassert>
+
+using namespace std;
+
+// ================================ ДОМАШНЕЕ ЗАДАНИЕ № 1 ================================
+
+// Создать абстрактный класс Figure(фигура).
+// Его наследниками являются классы Parallelogram(параллелограмм) и Circle(круг).
+// Класс Parallelogram — базовый для классов Rectangle(прямоугольник), Square(квадрат), Rhombus(ромб).
+// Для всех классов создать конструкторы.Для класса Figure добавить чисто виртуальную функцию area() (площадь).
+// Во всех остальных классах переопределить эту функцию, исходя из геометрических формул нахождения площади.
+
+class Figure  // фигура
+{
+public:
+    virtual double area() = 0;
+};
+
+class Circle : public Figure // круг
+{
+private:
+    double m_radius;
+public:
+    Circle(double rad): m_radius(rad) {}
+    double area() { return numbers::pi * pow(m_radius,2); }
+};
+
+class Parallelogram : public Figure // параллелограмм
+{
+protected:
+    double m_side;
+    double m_height;
+    Parallelogram (double side, double height) : m_side(side),m_height(height) {}
+};
+
+class Rectangle : public Parallelogram // прямоугольник
+{
+public:
+    Rectangle(double side, double height) : Parallelogram(side,height){}
+    double area() override { return m_side * m_height; }
+};
+
+class Square : public Parallelogram // квадрат
+{
+public:
+    Square(double side) : Parallelogram(side, side) {}
+    double area() override { return m_side * m_height; }
+};
+
+class Rhombus : public Parallelogram //ромб
+{
+public:
+    Rhombus(double side, double height) : Parallelogram(side, height) {}
+    double area() override { return m_side * m_height; }
+};
+
+// ================================ ДОМАШНЕЕ ЗАДАНИЕ № 2 ================================
+
+// Создать класс Car(автомобиль) с полями company(компания) и model(модель).
+// Классы - наследники: PassengerCar(легковой автомобиль) и Bus(автобус).
+// От этих классов наследует класс Minivan(минивэн).
+// Создать конструкторы для каждого из классов, чтобы они выводили данные о классах.
+// Создать объекты для каждого из классов и посмотреть, в какой последовательности выполняются конструкторы.
+// Обратить внимание на проблему «алмаз смерти».
+
+class Car
+{
+protected:
+    string m_company;
+    string m_model;
+public:
+    Car(string company, string model): m_company(company), m_model(model) 
+    { cout << "Автомобиль          компании: " << m_company << " | Модель: " << m_model << endl; }
+};
+
+class PassengerCar : virtual public Car
+{
+public:
+    PassengerCar(string company, string model) : Car(company, model) 
+    { cout << "Легковой автомобиль компании: " << m_company << " | Модель: " << m_model << endl; }
+    //~PassengerCar() { cout << "~ PassengerCar" << endl; }
+};
+
+class Bus : virtual public Car
+{
+public:
+    Bus(string company, string model) : Car(company, model) 
+    { cout << "Автобус             компании: " << m_company << " | Модель: " << m_model << endl; }
+    //~Bus() { cout << "~ Bus" << endl; }
+};
+
+class Minivan : public PassengerCar, public Bus
+{
+public:
+    Minivan(string company, string model) : Car(company, model), PassengerCar(company, model), Bus(company, model) 
+    { cout << "Минивен             компании: " << m_company << " | Модель: " << m_model << endl; }
+    //~Minivan() { cout << "~ Minivan" << endl; }
+};
+
+// ================================ ДОМАШНЕЕ ЗАДАНИЕ № 3 ================================
+
+// Создать класс : Fraction(дробь).Дробь имеет числитель и знаменатель(например, 3 / 7 или 9 / 2).Предусмотреть, чтобы знаменатель не был равен 0. 
+// Перегрузить : математические бинарные операторы(+, -, *, / ) для выполнения действий с дробями 
+// унарный оператор(-) логические операторы сравнения двух дробей(== , != , <, >, <= , >= ).
+// Примечание : Поскольку операторы < и >= , > и <= — это логические противоположности, попробуйте перегрузить один через другой.
+// Продемонстрировать использование перегруженных операторов.
+
+class Fraction
+{
+private:
+    double m_numerator;
+    double m_denominator;
+public:
+    Fraction(double numerator, double denominator) : m_numerator(numerator), m_denominator(denominator) { assert(denominator != 0); }
+    void printFraction()
+    {
+        if (m_numerator == 0)
+        {
+            cout << "0" << endl;
+        }
+        else if (m_numerator == m_denominator)
+        {
+            cout << "1" << endl;
+        }
+        else
+        {
+            cout << m_numerator << "/" << m_denominator << endl;
+        }
+    }
+    
+    friend Fraction operator +  (Fraction& fr_1, Fraction& fr_2);
+    friend Fraction operator -  (Fraction& fr_1, Fraction& fr_2);
+    friend Fraction operator *  (Fraction& fr_1, Fraction& fr_2);
+    friend Fraction operator /  (Fraction& fr_1, Fraction& fr_2);
+    friend bool     operator == (Fraction& fr_1, Fraction& fr_2);
+    friend bool     operator != (Fraction& fr_1, Fraction& fr_2);
+    friend bool     operator <  (Fraction& fr_1, Fraction& fr_2);
+    friend bool     operator >  (Fraction& fr_1, Fraction& fr_2);
+    friend bool     operator <= (Fraction& fr_1, Fraction& fr_2);
+    friend bool     operator >= (Fraction& fr_1, Fraction& fr_2);
+    Fraction        operator -  () const;
+};
+
+Fraction Fraction::operator - () const
+{
+    return Fraction(-m_numerator, m_denominator);
+}
+
+Fraction operator + (Fraction& fr_1, Fraction& fr_2)
+{
+    const double new_numerator   { fr_1.m_numerator * fr_2.m_denominator + fr_2.m_numerator * fr_1.m_denominator };
+    const double new_denominator { fr_1.m_denominator * fr_2.m_denominator };
+    return Fraction(new_numerator, new_denominator);
+}
+
+Fraction operator - (Fraction& fr_1, Fraction& fr_2)
+{
+    const double new_numerator   { fr_1.m_numerator * fr_2.m_denominator - fr_2.m_numerator * fr_1.m_denominator };
+    const double new_denominator { fr_1.m_denominator * fr_2.m_denominator };
+    return Fraction(new_numerator, new_denominator);
+}
+
+Fraction operator * (Fraction& fr_1, Fraction& fr_2)
+{
+    const double new_numerator   { fr_1.m_numerator * fr_2.m_numerator };
+    const double new_denominator { fr_1.m_denominator * fr_2.m_denominator };
+    return Fraction(new_numerator, new_denominator);
+}
+
+Fraction operator / (Fraction& fr_1, Fraction& fr_2)
+{
+    const double new_numerator   { fr_1.m_numerator * fr_2.m_denominator };
+    const double new_denominator { fr_1.m_denominator * fr_2.m_numerator };
+    return Fraction(new_numerator, new_denominator);
+}
+
+bool operator == (Fraction& fr_1, Fraction& fr_2)
+{
+    return fr_1.m_numerator / fr_1.m_denominator == fr_2.m_numerator / fr_2.m_denominator;
+}
+
+bool operator != (Fraction& fr_1, Fraction& fr_2)
+{
+    return !(fr_2 == fr_1);
+}
+
+bool operator < (Fraction& fr_1, Fraction& fr_2)
+{
+    return fr_1.m_numerator * fr_2.m_denominator < fr_2.m_numerator* fr_1.m_denominator;
+}
+
+bool operator > (Fraction& fr_1, Fraction& fr_2)
+{
+    return fr_1.m_numerator * fr_2.m_denominator > fr_2.m_numerator * fr_1.m_denominator;
+}
+
+bool operator <= (Fraction& fr_1, Fraction& fr_2)
+{
+    return !(fr_2 > fr_1);
+}
+
+bool operator >= (Fraction& fr_1, Fraction& fr_2)
+{
+    return !(fr_1 < fr_2);
+}
+
+// ================================ ДОМАШНЕЕ ЗАДАНИЕ № 4 ================================
+
+// Создать класс Card, описывающий карту в игре БлэкДжек.У этого класса должно быть три поля : масть, значение карты и положение карты(вверх лицом или рубашкой).
+// Сделать поля масть и значение карты типом перечисления(enum).
+// Положение карты - тип bool.
+// Также в этом классе должно быть два метода :метод Flip(), который переворачивает карту, т.е.если она была рубашкой вверх, то он ее поворачивает лицом вверх, и наоборот.
+// метод GetValue(), который возвращает значение карты, пока можно считать, что туз = 1.
+
+class Card
+{
+protected:
+
+    enum Card_Rank
+    {
+        RANK_2,
+        RANK_3,
+        RANK_4,
+        RANK_5,
+        RANK_6,
+        RANK_7,
+        RANK_8,
+        RANK_9,
+        RANK_10,
+        RANK_JACK,
+        RANK_QUEEN,
+        RANK_KING,
+        RANK_ACE        
+    };
+
+    enum Card_Suit
+    {
+        SUIT_CLUB,
+        SUIT_DIAMOND,
+        SUIT_HEART,
+        SUIT_SPADE,
+    };
+    
+    Card_Rank m_Rank;
+    Card_Suit m_Suit;
+    bool m_IsFaceUp = false;
+
+public:
+
+    Card(Card_Rank rank, Card_Suit suit, bool IsFaceUp) : m_Rank(rank), m_Suit(suit), m_IsFaceUp(IsFaceUp) {}
+
+    void Flip()
+    {
+        if (m_IsFaceUp)
+            m_IsFaceUp = true;
+        else
+            m_IsFaceUp = false;
+    }
+
+    int GetValue()
+    {
+        switch (m_Rank)
+        {
+        case RANK_2:	 return 2;
+        case RANK_3:	 return 3;
+        case RANK_4:	 return 4;
+        case RANK_5:	 return 5;
+        case RANK_6:	 return 6;
+        case RANK_7:	 return 7;
+        case RANK_8:	 return 8;
+        case RANK_9:	 return 9;
+        case RANK_10:	 return 10;
+        case RANK_JACK:	 return 10;
+        case RANK_QUEEN: return 10;
+        case RANK_KING:	 return 10;
+        case RANK_ACE:	 return 11;
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    // ================================ ДОМАШНЕЕ ЗАДАНИЕ № 1 ================================
+
+    cout << "  ========== Домашнее задание № 1 ========== " << endl << endl;
+
+    Circle    sCircle    (3);
+    Rectangle sRectangle (2, 4);
+    Square    sSquare    (5);
+    Rhombus   sRhombus   (5, 3);
+    cout << "Площадь круга          = " << sCircle.area()    << endl
+         << "Площадь прямоугольникa = " << sRectangle.area() << endl
+         << "Площадь квадратa       = " << sSquare.area()    << endl
+         << "Площадь ромбa          = " << sRhombus.area()   << endl;
+
+    // ================================ ДОМАШНЕЕ ЗАДАНИЕ № 2 ================================
+    
+    cout << endl << "  ========== Домашнее задание № 2 ========== " << endl << endl;
+
+    Car          Car  ("Лада", "калина");
+    PassengerCar pCar ("Лада", "гранта");
+    Bus          bCar ("ПАЗ ", "аврора");
+    Minivan      mCar ("Лада", "Ларгус");
+
+    // ================================ ДОМАШНЕЕ ЗАДАНИЕ № 3 ================================
+
+    cout << endl << "  ========== Домашнее задание № 3 ========== " << endl << endl;
+
+    Fraction fr_1(2, 1);
+    fr_1.printFraction();
+    Fraction fr_2(3, 4);
+    fr_2.printFraction();
+    Fraction fr_3 = fr_1 + fr_2;
+    fr_3.printFraction();
+    fr_3 = fr_1 - fr_2;
+    fr_3.printFraction();
+    fr_3 = fr_1 * fr_2;
+    fr_3.printFraction();
+    fr_3 = fr_1 / fr_2;
+    fr_3.printFraction();
+    fr_3 = -fr_1;
+    fr_3.printFraction();
+    fr_3 = fr_1;
+   
+    if (fr_1 == fr_3)
+    {
+        cout << "Дроби равны" << endl;
+    }
+    if (fr_1 != fr_2)
+    {
+        cout << "Дроби не равны" << endl;
+    }
+    if (fr_1 > fr_2)
+    {
+        cout << "Первая дробь больше втрой" << endl;
+    }
+   
+    cout << endl;
+}
